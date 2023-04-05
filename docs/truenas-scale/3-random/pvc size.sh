2d13e44c8c18de7c5a38f43ec4952e48bc634ebb
ZasX <zasxxsaz@gmail.com>
2023-04-05 02:00:59 +0200

    add pvc size

---
+++ docs/truenas-scale/3-random/pvc size.sh
@@ -0,0 +1,5 @@
+# PVC size
+
+```bash
+zfs list $(cli -c 'app kubernetes config' | grep -E "pool\s\|" | awk -F '|' '{print $3}' | tr -d " \t\n\r")/ix-applications/releases -r | grep pvc | awk '{print $1, $2}' | sort -hr -k2 | column -t
+```
